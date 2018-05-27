dcd14b46c37e4f66216c06df89205e44a65b5d64
Unknown <msacko99@yahoo.com>
2018-05-27 14:52:30 -0400

    Product refactoring and test

--- TAKADAM/Takadam/Takadam/Controllers/HomeController.cs
+++ TAKADAM/Takadam/Takadam/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Takadam.Models;
 
 namespace Takadam.Controllers
 {
@@ -23,6 +24,26 @@ namespace Takadam.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            //get All
+            List<Product> allProduct = Product.GetAll();
+
+            //Get By Id
+            Product prod = Product.GetById(1);
+
+            //Get By GetByCategoryId
+            List<Product> byCateg = Product.GetByCategoryId(1);
+
+            //Get By GetByStatus
+            List<Product> byStatus = Product.GetByStatus("eget tincidunt");
+
+            //Get By GetByCountry
+            List<Product> byCountry = Product.GetByCountry("Brazil");
 
             return View();
         }
--- TAKADAM/Takadam/Takadam/Models/Product.cs
+++ TAKADAM/Takadam/Takadam/Models/Product.cs
@@ -14,15 +14,23 @@ namespace Takadam.Models
         public string name { get; set; }
         public string description { get; set; }
         public int categoryId { get; set; }
-        public int price { get; set; }
+        public string price { get; set; }
         public string status { get; set; }
         public int nbStock { get; set; }
         public string country { get; set; }
 
+        public int memberId { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string memberCountry { get; set; }
+
+        public DateTime dateAjout { get; set; }
+        public string statusAjout { get; set; }
+
 
         private static List<Product> get(string query, Dictionary<string, object> parameters)
-        {
-            Product item = null;
+        {      
             Connection cs = new Connection();
             List<Product> list = new List<Product>();
 
@@ -30,7 +38,7 @@ namespace Takadam.Models
 
             while (reader.Read())
             {
-                item = new Product();
+                Product item = new Product();
 
                 if (!reader.IsDBNull(reader.GetOrdinal("id")))
                     item.id = reader.GetInt32(reader.GetOrdinal("id"));
@@ -47,17 +55,42 @@ namespace Takadam.Models
                 if (!reader.IsDBNull(reader.GetOrdinal("categoryId")))
                     item.categoryId = reader.GetInt32(reader.GetOrdinal("categoryId"));
 
-                if (!reader.IsDBNull(reader.GetOrdinal("price")))
-                    item.categoryId = reader.GetInt32(reader.GetOrdinal("price"));
+                if (!(reader["price"] == DBNull.Value))
+                    item.price = reader["price"].ToString().Trim();
 
                 if (!(reader["status"] == DBNull.Value))
                     item.status = reader["status"].ToString().Trim();
 
                 if (!reader.IsDBNull(reader.GetOrdinal("nbStock")))
-                    item.categoryId = reader.GetInt32(reader.GetOrdinal("nbStock"));
+                    item.nbStock = reader.GetInt32(reader.GetOrdinal("nbStock"));
 
                 if (!(reader["country"] == DBNull.Value))
                     item.country = reader["country"].ToString().Trim();
+
+
+                //Member Info
+                if (!reader.IsDBNull(reader.GetOrdinal("memberId")))
+                    item.memberId = reader.GetInt32(reader.GetOrdinal("memberId"));
+
+                if (!(reader["firstname"] == DBNull.Value))
+                    item.firstname = reader["firstname"].ToString().Trim();
+
+                if (!(reader["lastname"] == DBNull.Value))
+                    item.lastname = reader["lastname"].ToString().Trim();
+
+                if (!(reader["email"] == DBNull.Value))
+                    item.email = reader["email"].ToString().Trim();
+
+                if (!(reader["memberCountry"] == DBNull.Value))
+                    item.memberCountry = reader["memberCountry"].ToString().Trim();
+
+
+                //Member product Info
+                if (!(reader["statusAjout"] == DBNull.Value))
+                    item.statusAjout = reader["statusAjout"].ToString().Trim();
+
+                if (!reader.IsDBNull(reader.GetOrdinal("dateAjout")))
+                    item.dateAjout = reader.GetDateTime(reader.GetOrdinal("dateAjout"));
 
                 list.Add(item);
             }
@@ -66,17 +99,19 @@ namespace Takadam.Models
             return list;
         }
 
+
         public static List<Product> GetAll()
         {
-            string query = @"SELECT * from product";
+            string query = @"SELECT p.*, mp.status as statusAjout, mp.date as dateAjout, m.id as memberId, m.firstname, m.lastname, m.email, m.country as memberCountry from product p inner join memberproduct mp on p.id = mp.productId inner join member m on mp.memberId = m.id";
             Dictionary<string, object> param = new Dictionary<string, object>();
 
             return get(query, param);
         }
 
+
         public static Product GetById(int id)
         {
-            string query = @"SELECT * from product where id = @id";
+            string query = @"SELECT p.*, mp.status as statusAjout, mp.date as dateAjout, m.id as memberId, m.firstname, m.lastname, m.email, m.country as memberCountry from product p inner join memberproduct mp on p.id = mp.productId inner join member m on mp.memberId = m.id where p.id = @id";
             Dictionary<string, object> param = new Dictionary<string, object>();
             param.Add("id", id);
 
@@ -88,7 +123,7 @@ namespace Takadam.Models
 
         public static List<Product> GetByCategoryId(int categoryId)
         {
-            string query = @"SELECT * from product where categoryId = @categoryId";
+            string query = @"SELECT p.*, mp.status as statusAjout, mp.date as dateAjout, m.id as memberId, m.firstname, m.lastname, m.email, m.country as memberCountry from product p inner join memberproduct mp on p.id = mp.productId inner join member m on mp.memberId = m.id where p.categoryId = @categoryId";
             Dictionary<string, object> param = new Dictionary<string, object>();
             param.Add("categoryId", categoryId);
 
@@ -98,7 +133,7 @@ namespace Takadam.Models
 
         public static List<Product> GetByStatus(string status)
         {
-            string query = @"SELECT * from product where status = @status";
+            string query = @"SELECT p.*, mp.status as statusAjout, mp.date as dateAjout, m.id as memberId, m.firstname, m.lastname, m.email, m.country as memberCountry from product p inner join memberproduct mp on p.id = mp.productId inner join member m on mp.memberId = m.id where p.status = @status";
             Dictionary<string, object> param = new Dictionary<string, object>();
             param.Add("status", status);
 
@@ -108,7 +143,7 @@ namespace Takadam.Models
 
         public static List<Product> GetByCountry(string country)
         {
-            string query = @"SELECT * from product where country = @country";
+            string query = @"SELECT p.*, mp.status as statusAjout, mp.date as dateAjout, m.id as memberId, m.firstname, m.lastname, m.email, m.country as memberCountry from product p inner join memberproduct mp on p.id = mp.productId inner join member m on mp.memberId = m.id where p.country = @country";
             Dictionary<string, object> param = new Dictionary<string, object>();
             param.Add("country", country);
 
